fbe70cf15ae6e78ee4406dafd4111a6111fe83f3
Thomas Hayes <hayes.thomas610@gmail.com>
2023-01-03 17:23:30 -0700

    add spec for CustomerSubscription relationships and validations

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -2,6 +2,6 @@ class CustomerSubscription < ApplicationRecord
   belongs_to :customer
   belongs_to :subscription
 
-  validates :cusomter_id, presence: true
+  validates :customer_id, presence: true
   validates :subscription_id, presence: true
 end
